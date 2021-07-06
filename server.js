e81e2ad9aed857c4211297a9ae5993f8e7afc839
anuragchoudhary1910 <anuragchoudhary1910@gmail.com>
2021-07-07 04:08:25 +0530

    cors

--- server.js
+++ server.js
@@ -29,20 +29,12 @@ const corsOptions = {
 };
 //middlewares
 app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  app.header('Access-Control-Allow-Origin', '*');
+  app.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  app.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  app.header('Access-Control-Allow-Credentials', true);
   next();
 });
 app.use(morgan('dev'));
