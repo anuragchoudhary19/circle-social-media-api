b094c9080fa307ad59b1cd68499abb5dac958e53
anuragchoudhary1910 <anuragchoudhary1910@gmail.com>
2021-07-07 17:07:38 +0530

    cors

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var corsOptions = {
   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
   credentials: true,
 };
-app.use((req, res, next) => {
+app.all('/', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
