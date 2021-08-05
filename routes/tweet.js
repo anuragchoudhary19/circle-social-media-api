4e1591d9f7941a0636678bfbc035b0c51e175986
anuragchoudhary1910 <anuragchoudhary1910@gmail.com>
2021-08-05 08:39:13 +0530

    change tweet methods

--- routes/tweet.js
+++ routes/tweet.js
@@ -30,9 +30,9 @@ router.get('/tweet/comments/:id', jwtCheck, authCheck, getTweetComments);
 router.delete('/tweet/:id', jwtCheck, authCheck, remove);
 // router.get('/status/comments/:id', jwtCheck, authCheck, readComments);
 
-router.post('/tweet/like/:id', jwtCheck, authCheck, like);
-router.post('/tweet/retweet/:id', jwtCheck, authCheck, retweet);
-router.post('/tweet/comment/:id', jwtCheck, authCheck, commentOnTweet);
+router.put('/tweet/like/:id', jwtCheck, authCheck, like);
+router.put('/tweet/retweet/:id', jwtCheck, authCheck, retweet);
+router.put('/tweet/comment/:id', jwtCheck, authCheck, commentOnTweet);
 // router.delete('/tweet/comment/:id', jwtCheck, authCheck, removeComment);
 
 //any routes containing :userId,then app will first execute userById()
